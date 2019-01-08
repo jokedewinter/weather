753f2bce3c7bedb733351b7234923a0123d22c4a
Joke De Winter <joke@jokedewinter.co.uk>
2019-01-08 09:51:23 +0000

    Replacing post with POST

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Pages
 # -------------------------------------------------------
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def index():
     from engine import Location
 
