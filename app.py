baad8b0f63a17934d8da6540afbcf69124474525
Joke De Winter <joke@jokedewinter.co.uk>
2019-01-08 15:57:32 +0000

    Lost track

--- app.py
+++ app.py
@@ -11,33 +11,33 @@ app = Flask(__name__)
 # Functions
 # -------------------------------------------------------
 def log_file_exists(path):
-	"""
+    """
     Check if the log file exists and can be openend
-	"""
-	try:
-		f = open(path)
-		f.close()
-	except IOError:
-		return False
-	return True
+    """
+    try:
+        f = open(path)
+        f.close()
+    except IOError:
+        return False
+    return True
 
 
 def log_request(request, where, forecast):
-	"""
-	Log the location requests and write them to a log file
-	"""
-	from time import gmtime, strftime
-	current = strftime("%d/%m/%Y %H:%M:%S", gmtime())
+    """
+    Log the location requests and write them to a log file
+    """
+    from time import gmtime, strftime
+    current = strftime("%d/%m/%Y %H:%M:%S", gmtime())
 
-	with open('locations.log', 'a') as log:
-		#print(request.remote_addr, file=log) # IP address
-		#print(request.user_agent, file=log) # User agent
-		#print(where.title(), current, forecast, file=log, sep=' | ')
-		
-		temperature = str(forecast['temperature']) + '&#176;C'
-		weather = forecast['weather']
-		print(where.title(), current, temperature, weather, file=log, sep=' | ')
-		
+    with open('locations.log', 'a') as log:
+        #print(request.remote_addr, file=log) # IP address
+        #print(request.user_agent, file=log) # User agent
+        #print(where.title(), current, forecast, file=log, sep=' | ')
+
+        temperature = str(forecast['temperature']) + '&#176;C'
+        weather = forecast['weather']
+        print(where.title(), current, temperature, weather, file=log, sep=' | ')
+
 
 # -------------------------------------------------------
 # Pages
@@ -51,6 +51,11 @@ def index():
         where = Location(form_data["location"])
         forecast = where.get_location_data()
         log_request(request, form_data["location"], forecast)
+
+        if (forecast == 404) and (len(forecast) < 0):
+            wrong_location = form_data["location"]
+            return render_template("change.html", title="Change location", **locals())
+
     else:
         import geocoder
         g = geocoder.ip('me')
@@ -58,11 +63,8 @@ def index():
             where = Location('', g.latlng[0], g.latlng[1])
             forecast = where.get_ip_data()
 
-    if forecast != 404:
-        return render_template("index.html", title="Home", **locals())
-    else:
-        wrong_location = form_data["location"]
-        return render_template("change.html", title="Change location", **locals())
+
+    return render_template("index.html", title="Home", **locals())
 
 
 @app.route("/change")
@@ -72,23 +74,23 @@ def change():
 
 @app.route("/viewlog")
 def view_log():
-	"""
-	The import html is needed to unescape the HTML character for degrees.
-	"""
-	import html
-	contents = []
-	
-	if log_file_exists('locations.log') :
-	
-		with open('locations.log') as log:
-			#contents = log.read()
-			for line in log:
-				contents.append([])
-				for item in line.split('|'):
-					contents[-1].append(html.unescape(item))
-	
-	return render_template("viewlog.html", title="Logs", contents=contents)
-	
+    """
+    The import html is needed to unescape the HTML character for degrees.
+    """
+    import html
+    contents = []
+
+    if log_file_exists('locations.log') :
+
+        with open('locations.log') as log:
+            #contents = log.read()
+            for line in log:
+                contents.append([])
+                for item in line.split('|'):
+                    contents[-1].append(html.unescape(item))
+
+    return render_template("viewlog.html", title="Logs", contents=contents)
+
 
 @app.route("/about")
 def about():
